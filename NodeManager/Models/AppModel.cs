992eafbf45a5746723f61fe04869486afee4f7ae
Azhe Kun <azhe403@gmail.com>
2020-05-07 13:58:21 +0700

    rename to AppModel

---
+++ NodeManager/Models/AppModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Forms;
+using Prism.Mvvm;
+
+namespace NodeManager.Models
+{
+    internal class AppModel : BindableBase
+    {
+        private static AppModel _appModel;
+        private bool _isAppIdle;
+
+        public static NotifyIcon AppTrayIcon { get; set; }
+
+        public bool IsAppIdle
+        {
+            get => _isAppIdle;
+            set => SetProperty(ref _isAppIdle, value);
+        }
+
+        public AppModel()
+        {
+        }
+
+        public static AppModel GeInstance()
+        {
+            if (_appModel == null)
+            {
+                _appModel = new AppModel();
+            }
+
+            return _appModel;
+        }
+    }
+}
