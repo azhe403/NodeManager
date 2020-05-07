5d53a3e5d98f15b713652abd2bdb84d7a1269e80
Azhe Kun <azhe403@gmail.com>
2020-05-07 15:24:42 +0700

    add notify helper

---
+++ NodeManager/Helpers/NotifyHelper.cs
@@ -0,0 +1,38 @@
+﻿using NodeManager.Models;
+using System.Windows.Forms;
+
+namespace NodeManager.Helpers
+{
+    internal static class NotifyHelper
+    {
+        public static NotifyIcon Info(string message)
+        {
+            var trayIcon = AppModel.AppTrayIcon;
+            trayIcon.BalloonTipIcon = ToolTipIcon.Info;
+            trayIcon.BalloonTipText = message;
+            trayIcon.BalloonTipTitle = "NodeJS Manager";
+            trayIcon.ShowBalloonTip(3000);
+            return trayIcon;
+        }
+
+        public static NotifyIcon Warn(string message)
+        {
+            var trayIcon = AppModel.AppTrayIcon;
+            trayIcon.BalloonTipIcon = ToolTipIcon.Warning;
+            trayIcon.BalloonTipText = message;
+            trayIcon.BalloonTipTitle = "NodeJS Manager";
+            trayIcon.ShowBalloonTip(3000);
+            return trayIcon;
+        }
+
+        public static NotifyIcon Error(string message)
+        {
+            var trayIcon = AppModel.AppTrayIcon;
+            trayIcon.BalloonTipIcon = ToolTipIcon.Warning;
+            trayIcon.BalloonTipText = message;
+            trayIcon.BalloonTipTitle = "NodeJS Manager";
+            trayIcon.ShowBalloonTip(3000);
+            return trayIcon;
+        }
+    }
+}
