b9fd62116f5a1fd3a6e9a3d4f61e1d7fa470fd83
Azhe-kun <azhe403@gmail.com>
2020-05-21 15:47:13 +0700

    move to ZipHelper

--- NodeManager/Helpers/IOHelper.cs
+++ NodeManager/Helpers/IOHelper.cs
@@ -1,4 +1,4 @@
-﻿using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Zip;
 using Serilog;
 using System;
 using System.IO;
@@ -205,64 +205,5 @@ namespace NodeManager.Helpers
         }
 
         #endregion File
-
-        #region Zip Archiver
-
-        public static void FastUnzip(this string filePath, string destinationPath, bool createEmptyDir = false)
-        {
-            Log.Information($"Extracting file {filePath}");
-            var fastZip = new FastZip
-            {
-                CreateEmptyDirectories = createEmptyDir
-            };
-
-            // Will always overwrite if target filename s already exist
-            fastZip.ExtractZip(filePath, destinationPath, null);
-            Log.Information($"Extracted to {destinationPath}");
-        }
-
-        public static async Task FastUnzipAsync(this string filePath, string destinationPath,
-            bool createEmptyDir = false)
-        {
-            await Task.Run(() => { FastUnzip(filePath, destinationPath, createEmptyDir); });
-        }
-
-        public static bool IsZipValid(this string filePath)
-        {
-            bool isValid;
-
-            if (!filePath.IsFileExist())
-            {
-                Log.Information($"Zip Validation skipped because file {filePath} is not exist.");
-                return false;
-            }
-
-            try
-            {
-                Log.Information($"Validating {filePath}");
-                var zipFile = new ZipFile(filePath);
-                isValid = zipFile.TestArchive(true, TestStrategy.FindFirstError, null);
-                zipFile.Close();
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Error validating Zip");
-                isValid = false;
-            }
-
-            Log.Information($"Is {filePath} valid: {isValid}");
-            return isValid;
-        }
-
-        public static async Task<bool> IsZipValidAsync(this string filePath)
-        {
-            var isValid = false;
-
-            await Task.Run(() => { isValid = IsZipValid(filePath); });
-
-            return isValid;
-        }
-
-        #endregion Zip Archiver
     }
 }
